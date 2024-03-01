12cb488f0a485f8ac9d0e3a134bf7aa0aeec2881
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-03-02 00:13:51 +0200

    Update GScrappers.py

--- GScrappers.py
+++ GScrappers.py
@@ -106,7 +106,7 @@ class GScrappers():
             # Step 1: Get Page Content
             #--------------------------------------------------------------------------------
             HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-            response = requests.get(URL, headers=HEADERS, verify=True)
+            response = requests.get(URL, headers=HEADERS, verify=True, timeout=10)
             response.encoding = response.apparent_encoding
             
             try:
