ea599b9ad866bcfb06b32998bac84bca16256efe
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-03-01 21:14:46 +0200

    Add files via upload

---
+++ GScrappers.py
@@ -0,0 +1,247 @@
+import re
+import os
+import requests
+import pandas as pd
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+from Google_Parameters import google_params
+
+
+class GScrappers():
+    ###############################################################################################################################################################################################################
+    ######################################################################################### Google Functions ####################################################################################################
+    ###############################################################################################################################################################################################################
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Function 1 : Google Advanced URL Builder
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def Google_Advanced_URL(main_query, search_type='', exact_words='', any_of_words='', none_of_words='',number_from='', number_to='', site_or_domain='', results_count='',
+                            start_date='', end_date='', search_language='English', country='', term_apperaing='', file_type=''):
+
+        # Building Main Query
+        MAIN_QUERY = f"&as_q={main_query}".replace(" ","+")
+        EXACT_WORDS = f"&as_epq={" OR ".join(['"' + word + '"' for word in exact_words])}" if exact_words else ''
+        ANY_OF_WORDS = f"&as_oq={any_of_words}" if any_of_words else ''
+        NONE_OF_WORDS = f"&as_eq={"+".join(none_of_words)}" if none_of_words else ''
+        NUMBER_FROM = f"&as_nlo={number_from}" if number_from else ''
+        NUMBER_TO = f"&as_nhi={number_to}" if number_to else ''
+        SITE_OR_DOMAIN = f"&as_sitesearch={site_or_domain}" if site_or_domain else ''
+        RESULTS_COUNTS = f"&num={int(results_count)}" if results_count else ''
+        DATE = f"&tbs=cdr:1,cd_min:{start_date},cd_max:{end_date}" if start_date or end_date else ''
+        SEARCH_TYPE = google_params.search_type()[search_type] if search_type else ''
+        LANGUGAGE = google_params.languages()[search_language] if search_language else ''
+        COUNTRY = google_params.countries()[country] if country else ''
+        TERM_APPERAING = google_params.term_apperaing()[term_apperaing] if term_apperaing else ''
+        FILE_TYPE = google_params.file_type()[file_type] if file_type else ''
+        
+        # Building Final Search URL
+        if search_type != 'Scholar':
+            Url = f"https://www.google.com/search?{MAIN_QUERY}{EXACT_WORDS}{ANY_OF_WORDS}{NONE_OF_WORDS}{NUMBER_FROM}{NUMBER_TO}{SITE_OR_DOMAIN}{RESULTS_COUNTS}{DATE}{SEARCH_TYPE}{LANGUGAGE}{COUNTRY}{TERM_APPERAING}{FILE_TYPE}"
+        else:
+            Url = f"https://scholar.google.com/scholar?&{MAIN_QUERY}{EXACT_WORDS}{ANY_OF_WORDS}{NONE_OF_WORDS}{NUMBER_FROM}{NUMBER_TO}{SITE_OR_DOMAIN}{RESULTS_COUNTS}{DATE}{LANGUGAGE}{COUNTRY}{TERM_APPERAING}{FILE_TYPE}"
+        
+        # Return URL
+        return Url
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Function 2 : Google News Extractor
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def Google_Results(Search_URLS,Search_Type=''):
+        response = requests.get(Search_URLS)
+        response.encoding = response.apparent_encoding
+        soup = BeautifulSoup(response.content,'html.parser')
+
+        Results = [a for a in soup.find_all("a", href=True) if 'data-ved' in a.attrs]
+        Final_Results = []
+        
+        
+        # Extarct From Google News
+        if Search_Type == 'News':
+            for result in Results:
+                Headline = result.find('h3').text
+                Date = result.find(class_="r0bn4c rQMQod").text
+                Link = unquote(result.attrs['href'].split("/url?q=")[1].split('&sa')[0])
+
+                page = {"Headline":Headline,
+                        "Date":Date,
+                        "Link":Link}
+                
+                Final_Results.append(page)
+                
+                
+        # Extarct From Open Search      
+        else:
+            for result in Results:
+                try:
+                    Headline = result.find('h3').text
+                    Link = unquote(result.attrs['href'].split("/url?q=")[1].split('&sa')[0])
+
+                    page = {"Headline":Headline,
+                            "Link":Link}
+                
+                    Final_Results.append(page)
+                except:
+                    pass
+            
+        Google_Results = pd.DataFrame(Final_Results).drop_duplicates(subset=['Link'])
+        
+        return Google_Results
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+    ###############################################################################################################################################################################################################
+    ######################################################################################### Scrapping Functions #################################################################################################
+    ###############################################################################################################################################################################################################
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Function 3 : Website Scrapper
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def Website_Scraper(URL,Keywords=''):
+        try:
+            #--------------------------------------------------------------------------------
+            # Step 1: Get Page Content
+            #--------------------------------------------------------------------------------
+            HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+            response = requests.get(URL,headers=HEADERS)
+            response.encoding = response.apparent_encoding
+            
+            try:
+                soup = BeautifulSoup(response.content,"html.parser")
+            except:
+                soup = BeautifulSoup(response.content,"html.parser",from_encoding="iso-8859-1")
+
+            
+            #--------------------------------------------------------------------------------
+            # Step 2: Get Page Paragraphs
+            #--------------------------------------------------------------------------------
+            Paragraphs = []
+            body_text = soup.find_all('p')
+            
+            for entry in body_text:
+                if len(entry.text) > 1:
+                    p = f"({body_text.index(entry)})" + " • " + entry.text.strip().title() + '\n'
+                    if p not in Paragraphs:
+                        Paragraphs.append(p)
+                        
+            #--------------------------------------------------------------------------------
+            # Step 3: Get Most Relevant Paragraphs
+            #--------------------------------------------------------------------------------
+            Most_Relevant_Paragraphs = []
+            if len(Keywords) != 0:
+                for keyword in Keywords:
+                    for paragraph in Paragraphs:
+                        if paragraph.lower() not in Most_Relevant_Paragraphs:
+                            if keyword.lower() in paragraph.lower():
+                                Most_Relevant_Paragraphs.append(paragraph)
+                        else:
+                            pass
+                Most_Relevant_Paragraphs = "\n".join(Most_Relevant_Paragraphs)
+            else:
+                Most_Relevant_Paragraphs = ""
+            
+            #--------------------------------------------------------------------------------
+            # Step 4: Keyword Analysis
+            #--------------------------------------------------------------------------------
+            Keywords_Analysis = []
+            Keywords_Totals = 0
+            if len(Keywords) > 0: 
+                for keyword in Keywords:
+                    keyword_count = "\n".join(Paragraphs).lower().count(keyword.lower())
+                    Keywords_Totals = Keywords_Totals + keyword_count
+                    Keywords_Analysis.append(" • " + keyword.title() + " : " + str(keyword_count))
+            """          
+            #--------------------------------------------------------------------------------
+            #################################################################################
+            # Step 5: AI Summarization
+            ### TO BE COMPLETED IN FUTURE ###
+            #################################################################################
+            """
+            #--------------------------------------------------------------------------------
+            # Step 6: Create DF entry
+            #--------------------------------------------------------------------------------
+            page = {
+                    "Paragraphs":"\n".join(Paragraphs),
+                    "Most Relevant Paragraphs":Most_Relevant_Paragraphs,
+                    "Keywords Analysis":"\n".join(Keywords_Analysis),
+                    "Keywords Total":Keywords_Totals,
+                    "Link":URL}
+   
+            #--------------------------------------------------------------------------------
+            
+        except Exception as e:
+            print(f'Error Faced in Web Content Scrapping: \n{e}')
+            page = {
+                "Paragraphs":"Site Wasn't Scrapped",
+                "Most Relevant Paragraphs":"Site Wasn't Scrapped",
+                "Keywords Analysis":"Site Wasn't Scrapped",
+                "Keywords Total":"Site Wasn't Scrapped",
+                "Link":URL}
+            
+            return e
+        return page 
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    
+    
+    ###############################################################################################################################################################################################################
+    ######################################################################################### Suppport Functions ##################################################################################################
+    ############################################################################################################################################################################################################### 
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Function 1 : Results Formating
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def Result_Formating(Google_Results,Scrapping_Results,Keywords=''):
+        # Merge Dataframes
+        Final_Results = pd.merge(Google_Results,Scrapping_Results,on='Link')
+        
+        # Drop Duplicates and Empty Columns
+        Final_Results = Final_Results.drop_duplicates(subset='Link').dropna(axis=1, how='all')
+        
+        # Calculate Relevancy Score
+        Total_Keywords = Final_Results['Keywords Total'].sum()
+        Final_Results['Relevancy Score'] = Final_Results['Keywords Total'] / Total_Keywords
+        
+        #Rearrange collumns
+        if len(Keywords) != 0:
+            try:
+                Final_Results = Final_Results[['Headline', 'Date', 'Paragraphs', 'Most Relevant Paragraphs', 'Keywords Analysis', 'Keywords Total', 'Relevancy Score', 'Link']]
+            except:
+                Final_Results = Final_Results[['Headline', 'Paragraphs', 'Most Relevant Paragraphs', 'Keywords Analysis', 'Keywords Total', 'Relevancy Score', 'Link']]
+            # Sort by Keywords Total
+            Final_Results = Final_Results.sort_values(by='Relevancy Score',ascending=False)
+        
+        else:
+            try:
+                Final_Results = Final_Results[['Headline', 'Date', 'Paragraphs', 'Link']]
+            except:
+                Final_Results = Final_Results[['Headline', 'Paragraphs', 'Link']]
+                
+        return Final_Results
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Function 2 : Save Excel
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def Save_Excel(Dataframe,excel_name='Results'):
+        directory = os.path.expanduser("~/Desktop/")
+        excel_name = re.sub(r'[<>:"/\\|?*]', '', excel_name)
+        
+        writer = pd.ExcelWriter(directory +'{}.xlsx'.format(excel_name),engine="xlsxwriter")
+        Dataframe.to_excel(writer,index=False,sheet_name='Results')
+
+        workbook = writer.book
+        worksheet = writer.sheets['Results']
+
+        worksheet.set_zoom(80)
+        cell_format = workbook.add_format({'text_wrap': True,"align" : "top","valign": "vtop"})
+
+        worksheet.set_column("A:Z",30,cell_format)
+        worksheet.set_default_row(17.5)
+        writer.close()
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    
---
+++ QueryCrawler.py
@@ -0,0 +1,112 @@
+import streamlit as st
+from streamlit_tags import st_tags, st_tags_sidebar
+from time import sleep
+from stqdm import stqdm
+from GScrappers import *
+from Google_Parameters import *
+import pandas as pd
+from datetime import datetime, timedelta
+import xlsxwriter
+import io
+
+###############################################################################################################################################################################################################
+# User Inputs
+###############################################################################################################################################################################################################
+st.title('Welcome to :blue[Query Catcher]')
+st.markdown('This tool has been developed for quick research and website scrapping')
+st.subheader('Start by defining your Search',divider='rainbow')
+Search_Type = st.radio('Search Type',pd.DataFrame(list({'Simple Search':1,'Advanced Search':2})),horizontal=True)
+Search_Query= st.text_input('Search Query')
+Search_Source = st.radio('Search Source',pd.DataFrame(list(google_params.search_type_basic())),horizontal=True)
+
+if Search_Type == 'Advanced Search': st.divider()
+
+Included_Keywords = st_tags(label='Included Keywords:', text='Press enter to add more', maxtags = 10, key='Included Keywords') if Search_Type == 'Advanced Search' else ""
+Discarded_Keywords= st_tags(label='Discarded Keywords:', text='Press enter to add more', maxtags = 10, key='Discarded Keywords') if Search_Type == 'Advanced Search' else ""
+
+if Search_Type == 'Advanced Search': st.divider()
+
+col1, col2 = st.columns(2)
+with col1:
+    Terms_Appearing = st.selectbox('Keywords Apeearning',google_params.term_apperaing().keys(),placeholder="Choose a Country") if Search_Type == 'Advanced Search' else ""
+    Country = st.selectbox('Country',google_params.countries().keys(),placeholder="Choose a Country") if Search_Type == 'Advanced Search' else ""
+    Language = st.selectbox('Language',google_params.languages().keys(),placeholder="Choose a search language") if Search_Type == 'Advanced Search' else ""
+
+with col2:
+    Date_Range = st.date_input ('Date Range',value=[],format="DD/MM/YYYY",help='Select start and end date for the search') if Search_Type == 'Advanced Search' else ""
+    Results_Quantity = st.number_input('Results Quantity',min_value=10,max_value=100) if Search_Type == 'Advanced Search' else ""
+
+Session = st.session_state
+
+###############################################################################################################################################################################################################
+# Extraction
+###############################################################################################################################################################################################################
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Section 1: Get Gogogle Results
+st.divider()
+col3,col4,col5,col6 = st.columns(4)
+with col3:
+    Start_Search = st.button ('Start Search')
+if Start_Search:
+    # Step 1: Prepare Search URL
+    Start_Date = Date_Range[0].strftime("%m-%d-%Y") if Search_Type == 'Advanced Search' else ""
+    End_Date = Date_Range[1].strftime("%m-%d-%Y")  if Search_Type == 'Advanced Search' else ""
+    Google_URL = GScrappers.Google_Advanced_URL(Search_Query, search_type=Search_Source, exact_words=Included_Keywords, any_of_words='', none_of_words=Discarded_Keywords,
+                                                number_from='', number_to='', site_or_domain='', results_count=Results_Quantity,
+                                                start_date=Start_Date, end_date=End_Date, search_language=Language, country=Country, term_apperaing=Terms_Appearing, file_type='')
+    st.write(Google_URL)
+    # Step 2: Get Google Search Results
+    Google_Results = GScrappers.Google_Results(Google_URL,Search_Source)
+    
+    st.data_editor(Google_Results,column_config={"Link":st.column_config.LinkColumn("Source",display_text='Link')},width=1280)
+    
+    # Step 3: Save results to Session
+    st.session_state['google_results'] = Google_Results
+    
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Section 2: Scrape Google Results
+if 'google_results' in Session.keys():
+    
+    # Step 1: Ask if User want to scrape
+    with col4:
+        Start_Scapping = st.button('Start Scapping')
+    if Start_Scapping:
+        # Get Links from Google Results
+        Links = Session['google_results']['Link']
+        
+        # Itterate over each link to scrape website and save in Scrapped_Websites_List
+        Scraped_Websites_List = []
+        for link in stqdm(Links):
+            Website_dict = GScrappers.Website_Scraper(link,Included_Keywords)
+            Scraped_Websites_List.append(Website_dict)
+        
+        # Create a Dataframe for scrapping results and save in Session   
+        Scraped_Websites_DF = pd.DataFrame(Scraped_Websites_List)
+        Session['Articles DF'] = Scraped_Websites_DF
+        
+        # Merge Scraped Websites with Google Results and Format the findings
+        Final_Results = GScrappers.Result_Formating(Session['google_results'],Scraped_Websites_DF,Included_Keywords)
+        st.data_editor(Final_Results,column_config={"Link":st.column_config.LinkColumn("Source",display_text='Link')})
+        Session['Final_Results'] = Final_Results
+        
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+if 'Final_Results' in Session.keys():
+        
+    with col5:
+        buffer = io.BytesIO()
+        df = Session['Final_Results']
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            # Write each dataframe to a different worksheet.
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+        Donwload = st.download_button('Download Results',data=buffer,file_name="Results.xlsx",mime='textcsv')
+        
+    with col6:
+        Refresh = st.button('Reset Research')
+        if Refresh:
+            for key in Session.keys():
+                del Session[key]
